67420727a7ad69f1672883af2ea925cdee41dbc0
Sydney <sydney3@stanford.edu>
2026-03-08 14:05:04 -0700

    new experiments more IFU

---
+++ Experiments/roman_forecast/InferenceRuns/expD1_config.py
@@ -0,0 +1,345 @@
+# experiment 1.2: Gold-Only Baseline, Human-Bias Selected
+
+import h5py
+import pandas as pd
+import numpy as np
+from scipy.stats import norm, multivariate_normal
+
+# random seed
+RANDOM_SEED = 1
+
+# file locations
+static_dv_file = 'InferenceRuns/expD1/static_datavectors_seed'+str(RANDOM_SEED)+'.json'
+
+# 3 modeling options...locations of samples from joint fermat/csqrt(J) posteriors
+quads_20perc_h5_file = 'DataVectors/lsst_catalog/from_sherlock/quad_posteriors_20percfpd.h5' # LSST
+quads_05perc_h5_file = 'DataVectors/lsst_catalog/from_sherlock/quad_posteriors_05percfpd.h5' # ROMAN
+quads_03perc_h5_file = 'DataVectors/lsst_catalog/from_sherlock/quad_posteriors_03percfpd.h5' # ADDITIONAL
+
+# 
+dbls_20perc_h5_file = 'DataVectors/lsst_catalog/from_sherlock/dbl_posteriors_20percfpd.h5' # LSST
+dbls_05perc_h5_file = 'DataVectors/lsst_catalog/from_sherlock/dbl_posteriors_05percfpd.h5' # ROMAN
+#dbls_03perc_h5_file = 'DataVectors/lsst_catalog/from_sherlock/dbl_posteriors_03percfpd.h5' # ADDITIONAL
+
+lsst_metadata_file = 'DataVectors/lsst_catalog/truth_metadata_popsigma05.csv'
+
+NUM_FPD_SAMPS = 5000
+NUM_MCMC_EPOCHS = 70000
+NUM_MCMC_WALKERS = 50
+COSMO_MODEL = 'LCDM_lambda_int_beta_ani'
+HI_REWEIGHTING = False
+OMEGA_M_PRIOR = True # !!!! INFORMATIVE Omega_M prior when True !!!!!
+# this beta_ani prior does affect the inference.
+BETA_ANI_PRIOR = norm(loc=0.,scale=0.2).logpdf
+# where to store the chain...
+BACKEND_PATH = 'InferenceRuns/expD1/LCDM_infOM_seed'+str(RANDOM_SEED)+'_backend.h5'
+RESET_BACKEND=True
+
+# assumed modeling prior
+mu_lp_gold = np.asarray([0.85,0.,0.,2.09,0.,0.,0.,0.,0.,0.]) # hst_norms.csv
+stddev_lp_gold = np.asarray([0.28,0.06,0.06,0.16,0.20,0.20,0.06,0.06,0.34,0.34])
+
+# truth information for those indices
+lsst_df = pd.read_csv(lsst_metadata_file)
+# NOTE: excluding bad lenses (bad paltas model, NaN kinematics computation, etc.)
+lsst_bad_idx = [ 'l00676' , # <- this one is a bad paltas model 
+    'l00113', 'l00322', 'l00389', 'l00525', 'l00586', 'l00659', 'l00823', 'l00902', # <- these are all 
+    'l01242' ,'l01297', 'l01312', 'l01385', 'l01410' ,'l01413', 'l01434', 'l01559', # Nan kinematics
+    'l01580', 'l01834', 'l01893', 'l01915', 'l02010', 'l02040', 'l02100', 'l02101',
+    'l02141', 'l02310', 'l02573', 'l02575' ,'l02586', 'l02587']
+
+
+lsst_df = lsst_df[~lsst_df['catalog_idx'].isin(lsst_bad_idx)].reset_index(drop=True)
+# track remaining catalog_idxs
+lsst_df_catalog_idxs = lsst_df.loc[:,'catalog_idx'].to_numpy()
+
+#########################
+# Human selection cuts!!
+#########################
+
+# use the random seed
+np.random.seed(RANDOM_SEED)
+
+################
+# 8 TDCOSMO LENSES (all quads)
+################
+num_tdcosmo_quads = 8
+nirspec_quads_avail = np.where(
+    (lsst_df['point_source_parameters_num_images'].to_numpy() == 4) &
+    ((np.abs(lsst_df['td01'].to_numpy()) > 10.) | 
+     (np.abs(lsst_df['td02'].to_numpy()) > 10.) | 
+     (np.abs(lsst_df['td03'].to_numpy()) > 10.)) &
+    (lsst_df['lens_light_parameters_mag_app'].to_numpy() < 24.) &
+    (lsst_df['source_parameters_mag_app'].to_numpy() < 24.)
+)[0]
+# take the catalog idxs you want
+#print('num tdcosmo quads available: ', nirspec_quads_avail)
+catalog_idx_avail = lsst_df.loc[nirspec_quads_avail,'catalog_idx'].to_numpy()
+tdcosmo_quads_catalog_idxs = np.random.choice(catalog_idx_avail,
+    size=num_tdcosmo_quads,replace=False)
+# then remove them from the dataframe
+lsst_df = lsst_df[~lsst_df['catalog_idx'].isin(tdcosmo_quads_catalog_idxs)].reset_index(drop=True)
+
+
+# 800 LSST lenses. 80 are quads, 720 are doubles (10% quad fraction starting from 800)
+num_lsst_aper_quads = 40
+num_lsst_only_quads = 40
+num_lsst_aper_dbls = 360 # is this too high?
+num_lsst_only_dbls = 360
+
+################
+# LSST Quads with aperture-kin (40)
+################
+lsst_aper_quads_avail = np.where(
+    (lsst_df['point_source_parameters_num_images'].to_numpy() == 4) &
+    ((np.abs(lsst_df['td01'].to_numpy()) > 10.) |  # 10-day LSST time-delay threshold
+     (np.abs(lsst_df['td02'].to_numpy()) > 10.) | 
+     (np.abs(lsst_df['td03'].to_numpy()) > 10.)) &
+    (lsst_df['lens_light_parameters_mag_app'].to_numpy() < 22.) # Bright enough for kinematics
+)[0]
+# edge case: not enough quads that pass this criteria, add to lsst-only bucket
+if len(lsst_aper_quads_avail)<num_lsst_aper_quads:
+    num_lsst_only_quads += (num_lsst_aper_quads - len(lsst_aper_quads_avail))
+    num_lsst_aper_quads = len(lsst_aper_quads_avail)
+# now sample the catalog idxs
+catalog_idx_avail = lsst_df.loc[lsst_aper_quads_avail,'catalog_idx'].to_numpy()
+lsst_aper_quads_catalog_idxs = np.random.choice(catalog_idx_avail,
+    size=num_lsst_aper_quads,replace=False)
+print('Including %d Lsst+Aper Quads'%(len(lsst_aper_quads_catalog_idxs)))
+# then remove them from the dataframe
+lsst_df = lsst_df[~lsst_df['catalog_idx'].isin(lsst_aper_quads_catalog_idxs)].reset_index(drop=True)
+
+################
+# LSST Quads w/out aperture-kin (40)
+################
+lsst_only_quads_avail = np.where(
+    (lsst_df['point_source_parameters_num_images'].to_numpy() == 4) &
+    ((np.abs(lsst_df['td01'].to_numpy()) > 10.) |  # 10-day LSST time-delay threshold
+     (np.abs(lsst_df['td02'].to_numpy()) > 10.) | 
+     (np.abs(lsst_df['td03'].to_numpy()) > 10.))
+)[0]
+# edge case: not enough quads that pass this criteria, add to lsst-only-doubles bucket
+if len(lsst_only_quads_avail)<num_lsst_only_quads:
+    num_lsst_only_dbls += (num_lsst_only_quads - len(lsst_only_quads_avail))
+    num_lsst_only_quads = len(lsst_only_quads_avail)
+# now sample the catalog idxs
+catalog_idx_avail = lsst_df.loc[lsst_only_quads_avail,'catalog_idx'].to_numpy()
+lsst_only_quads_catalog_idxs = np.random.choice(catalog_idx_avail,
+    size=num_lsst_only_quads,replace=False)
+print('Including %d Lsst-Only Quads'%(len(lsst_only_quads_catalog_idxs)))
+# then remove them from the dataframe
+lsst_df = lsst_df[~lsst_df['catalog_idx'].isin(lsst_only_quads_catalog_idxs)].reset_index(drop=True)
+
+################
+# LSST Doubles with aperture-kin (360)
+################
+lsst_aper_dbls_avail = np.where(
+    (lsst_df['point_source_parameters_num_images'].to_numpy() == 2) &
+    (np.abs(lsst_df['td01'].to_numpy()) > 10.) & # 10-day LSST time-delay threshold)
+    (lsst_df['lens_light_parameters_mag_app'].to_numpy() < 22.) # Bright enough for kinematics
+)[0]
+# edge case: not enough dbls that pass this criteria, add to lsst-only-doubles bucket
+if len(lsst_aper_dbls_avail)<num_lsst_aper_dbls:
+    num_lsst_only_dbls += (num_lsst_aper_dbls - len(lsst_aper_dbls_avail))
+    num_lsst_aper_dbls = len(lsst_aper_dbls_avail)
+# now sample the catalog idxs
+catalog_idx_avail = lsst_df.loc[lsst_aper_dbls_avail,'catalog_idx'].to_numpy()
+lsst_aper_dbls_catalog_idxs = np.random.choice(catalog_idx_avail,
+    size=num_lsst_aper_dbls,replace=False)
+print('Including %d Lsst+Aper Dbls'%(len(lsst_aper_dbls_catalog_idxs)))
+# then remove them from the dataframe
+lsst_df = lsst_df[~lsst_df['catalog_idx'].isin(lsst_aper_dbls_catalog_idxs)].reset_index(drop=True)
+
+################
+# LSST Doubles w/out aperture-kin (360)
+################
+lsst_only_dbls_avail = np.where(
+    (lsst_df['point_source_parameters_num_images'].to_numpy() == 2) &
+    (np.abs(lsst_df['td01'].to_numpy()) > 10.)  # 10-day LSST time-delay threshold)
+)[0]
+# edge case: not enough dbls that pass this criteria, total sample will be smaller
+if len(lsst_only_dbls_avail)<num_lsst_aper_dbls:
+    num_lsst_only_dbls = len(lsst_only_dbls_avail)
+# now sample the catalog idxs
+catalog_idx_avail = lsst_df.loc[lsst_only_dbls_avail,'catalog_idx'].to_numpy()
+lsst_only_dbls_catalog_idxs = np.random.choice(catalog_idx_avail,
+    size=num_lsst_only_dbls,replace=False)
+print('Including %d Lsst-Only Dbls'%(len(lsst_only_dbls_catalog_idxs)))
+# then remove them from the dataframe
+lsst_df = lsst_df[~lsst_df['catalog_idx'].isin(lsst_only_dbls_catalog_idxs)].reset_index(drop=True)
+
+
+########################################################
+# Upgrade 1/4 of these lenses with Roman imaging (5% fpd)
+########################################################
+
+# integer division for indexing
+num_rom_aper_quads = len(lsst_aper_quads_catalog_idxs) // 4
+num_rom_only_quads = len(lsst_only_quads_catalog_idxs) // 4
+num_rom_aper_dbls = len(lsst_aper_dbls_catalog_idxs) // 4
+num_rom_only_dbls = len(lsst_only_dbls_catalog_idxs) // 4
+
+##############################
+# Set-up inference configs
+##############################
+likelihood_configs = {
+
+    # TDCOSMO counterpart (8 IFU lenses)
+    'tdcosmo_quads':{
+        'posteriors_h5_file':quads_03perc_h5_file,
+        'metadata_file':lsst_metadata_file,
+        'catalog_idxs':tdcosmo_quads_catalog_idxs,
+        'cosmo_model':COSMO_MODEL,
+        'td_meas_error_percent':None,
+        'td_meas_error_days':1., # CHANGED TO 1-DAY MEAS. ERROR ALWAYS
+        'kappa_ext_meas_error_value':0.05,
+        'kinematic_type':'NIRSPEC',
+        'kin_meas_error_percent':0.03, # CHANGED TO 3% KINEMATICS
+        'kin_meas_error_kmpersec':None,
+        'num_gaussianized_samps':NUM_FPD_SAMPS,
+        'lens_params_nu_int_means':mu_lp_gold,
+        'lens_params_nu_int_stddevs':stddev_lp_gold,
+        'log_prob_beta_ani_nu_int':BETA_ANI_PRIOR
+    },
+
+    # roman aperture quads (modification: every Roman lens gets IFU)
+    'roman_aper_quads':{
+        'posteriors_h5_file':quads_05perc_h5_file, # UPGRADE TO 5% fpd
+        'metadata_file':lsst_metadata_file,
+        'catalog_idxs':lsst_aper_quads_catalog_idxs[:num_rom_aper_quads],
+        'cosmo_model':COSMO_MODEL,
+        'td_meas_error_percent':None,
+        'td_meas_error_days':3., # 3-Day ROMAN/LSST TD PREC. (B2 only)
+        'kappa_ext_meas_error_value':0.05,
+        'kinematic_type':'MUSE',
+        'kin_meas_error_percent':0.03, # 3% MUSE (3 bins)
+        'kin_meas_error_kmpersec':None,
+        'num_gaussianized_samps':NUM_FPD_SAMPS,
+        'lens_params_nu_int_means':mu_lp_gold,
+        'lens_params_nu_int_stddevs':stddev_lp_gold,
+        'log_prob_beta_ani_nu_int':BETA_ANI_PRIOR
+    },
+
+    # lsst quads with aperture kin (~30 lenses)
+    'lsst_aper_quads':{
+        'posteriors_h5_file':quads_20perc_h5_file,
+        'metadata_file':lsst_metadata_file,
+        'catalog_idxs':lsst_aper_quads_catalog_idxs[num_rom_aper_quads:],
+        'cosmo_model':COSMO_MODEL,
+        'td_meas_error_percent':None,
+        'td_meas_error_days':3., # 5-Day LSST Meas. Precision
+        'kappa_ext_meas_error_value':0.05,
+        'kinematic_type':'4MOST',
+        'kin_meas_error_percent':0.03, # 3% single-aperture
+        'kin_meas_error_kmpersec':None,
+        'num_gaussianized_samps':NUM_FPD_SAMPS,
+        'lens_params_nu_int_means':mu_lp_gold,
+        'lens_params_nu_int_stddevs':stddev_lp_gold,
+        'log_prob_beta_ani_nu_int':BETA_ANI_PRIOR
+    },
+
+    # quads without kinematics (modification: every Roman lens gets IFU)
+    'roman_only_quads':{
+        'posteriors_h5_file':quads_05perc_h5_file, # UPGRADE TO 5% fpd
+        'metadata_file':lsst_metadata_file,
+        'catalog_idxs':lsst_only_quads_catalog_idxs[:num_rom_only_quads],
+        'cosmo_model':COSMO_MODEL,
+        'td_meas_error_percent':None,
+        'td_meas_error_days':3., # 3-Day ROMAN/LSST TD PREC. (B2 only)
+        'kappa_ext_meas_error_value':0.05,
+        'kinematic_type':'MUSE',
+        'kin_meas_error_percent':0.03, # 3% MUSE (3 bins)
+        'kin_meas_error_kmpersec':None,
+        'num_gaussianized_samps':NUM_FPD_SAMPS,
+        'lens_params_nu_int_means':mu_lp_gold,
+        'lens_params_nu_int_stddevs':stddev_lp_gold,
+        'log_prob_beta_ani_nu_int':BETA_ANI_PRIOR
+    },
+
+    'lsst_only_quads':{
+        'posteriors_h5_file':quads_20perc_h5_file,
+        'metadata_file':lsst_metadata_file,
+        'catalog_idxs':lsst_only_quads_catalog_idxs[num_rom_only_quads:],
+        'cosmo_model':COSMO_MODEL,
+        'td_meas_error_percent':None,
+        'td_meas_error_days':3., # 3-Day LSST Meas. Precision
+        'kappa_ext_meas_error_value':0.05,
+        'kinematic_type':None,
+        'kin_meas_error_percent':None,
+        'kin_meas_error_kmpersec':None,
+        'num_gaussianized_samps':NUM_FPD_SAMPS,
+        'lens_params_nu_int_means':mu_lp_gold,
+        'lens_params_nu_int_stddevs':stddev_lp_gold,
+        'log_prob_beta_ani_nu_int':BETA_ANI_PRIOR
+    },
+
+    # doubles with kinematics (modification: every Roman lens gets IFU)
+    'roman_aper_dbls':{
+        'posteriors_h5_file':dbls_05perc_h5_file, # UPGRADE TO 5% fpd
+        'metadata_file':lsst_metadata_file,
+        'catalog_idxs':lsst_aper_dbls_catalog_idxs[:num_rom_aper_dbls],
+        'cosmo_model':COSMO_MODEL,
+        'td_meas_error_percent':None,
+        'td_meas_error_days':3., # 3-Day ROMAN/LSST TD PREC. (B2 only)
+        'kappa_ext_meas_error_value':0.05,
+        'kinematic_type':'MUSE',
+        'kin_meas_error_percent':0.03, # 3% MUSE (3 bins)
+        'kin_meas_error_kmpersec':None,
+        'num_gaussianized_samps':NUM_FPD_SAMPS,
+        'lens_params_nu_int_means':mu_lp_gold,
+        'lens_params_nu_int_stddevs':stddev_lp_gold,
+        'log_prob_beta_ani_nu_int':BETA_ANI_PRIOR
+    },
+
+    'lsst_aper_dbls':{
+        'posteriors_h5_file':dbls_20perc_h5_file,
+        'metadata_file':lsst_metadata_file,
+        'catalog_idxs':lsst_aper_dbls_catalog_idxs[num_rom_aper_dbls:],
+        'cosmo_model':COSMO_MODEL,
+        'td_meas_error_percent':None,
+        'td_meas_error_days':3., # 3-Day LSST Meas. Precision
+        'kappa_ext_meas_error_value':0.05,
+        'kinematic_type':'4MOST',
+        'kin_meas_error_percent':0.03, # 3% single-aperture
+        'kin_meas_error_kmpersec':None,
+        'num_gaussianized_samps':NUM_FPD_SAMPS,
+        'lens_params_nu_int_means':mu_lp_gold,
+        'lens_params_nu_int_stddevs':stddev_lp_gold,
+        'log_prob_beta_ani_nu_int':BETA_ANI_PRIOR
+    },
+
+    # doubles with no kinematics (modification: every Roman lens gets IFU)
+    'roman_only_dbls':{
+        'posteriors_h5_file':dbls_05perc_h5_file, # UPGRADE TO 5% fpd
+        'metadata_file':lsst_metadata_file,
+        'catalog_idxs':lsst_only_dbls_catalog_idxs[:num_rom_only_dbls],
+        'cosmo_model':COSMO_MODEL,
+        'td_meas_error_percent':None,
+        'td_meas_error_days':3., # 3-Day ROMAN/LSST TD PREC. (B2 only)
+        'kappa_ext_meas_error_value':0.05,
+        'kinematic_type':'MUSE',
+        'kin_meas_error_percent':0.03, # 3% MUSE (3 bins)
+        'kin_meas_error_kmpersec':None,
+        'num_gaussianized_samps':NUM_FPD_SAMPS,
+        'lens_params_nu_int_means':mu_lp_gold,
+        'lens_params_nu_int_stddevs':stddev_lp_gold,
+        'log_prob_beta_ani_nu_int':BETA_ANI_PRIOR
+    },
+
+    'lsst_only_dbls':{
+        'posteriors_h5_file':dbls_20perc_h5_file,
+        'metadata_file':lsst_metadata_file,
+        'catalog_idxs':lsst_only_dbls_catalog_idxs[num_rom_only_dbls:],
+        'cosmo_model':COSMO_MODEL,
+        'td_meas_error_percent':None,
+        'td_meas_error_days':3., # 3-Day LSST Meas. Precision
+        'kappa_ext_meas_error_value':0.05,
+        'kinematic_type':None,
+        'kin_meas_error_percent':None,
+        'kin_meas_error_kmpersec':None,
+        'num_gaussianized_samps':NUM_FPD_SAMPS,
+        'lens_params_nu_int_means':mu_lp_gold,
+        'lens_params_nu_int_stddevs':stddev_lp_gold,
+        'log_prob_beta_ani_nu_int':BETA_ANI_PRIOR
+    }
+}
--- Utils/mcmc_utils.py
+++ Utils/mcmc_utils.py
@@ -1,6 +1,104 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde
+import numpy as np
+import arviz as az
+
+# let's try to do an approximate FOM ...
+def DE_fom(test_chain,burnin):
+
+    test_chain = test_chain[:,burnin:,:].reshape((-1,test_chain.shape[2]))
+    w0wa_samps = test_chain[:,[2,3]]
+
+    # choose a random 10000 samps to fit from (there's way too many samples to do the fitting)
+    if np.shape(w0wa_samps)[0] > 100000:
+        idx_avail = np.arange(0,len(w0wa_samps))
+        idx_chosen = np.random.choice(idx_avail,100000,replace=False)
+        w0wa_samps_small = w0wa_samps[idx_chosen]
+    else:
+        w0wa_samps_small = w0wa_samps
+
+    # fit gaussian to the samps
+    Mean = np.mean(w0wa_samps_small,axis=0)
+    Cov = np.cov(w0wa_samps_small,rowvar=False)
+
+    # find the pivot value
+    one_minus_ap = - Cov[1,0] / Cov[1,1] # 1 - ap = - <delta_w0 delta_wa> / <delta_wa^2>
+    z_pivot = one_minus_ap / (-one_minus_ap+1)
+    print('pivot redshift: ', one_minus_ap / (-one_minus_ap+1))
+    # transform Mu and Cov
+    Transform = np.asarray([[1, one_minus_ap],[0, 1]])
+    Mean_pivot = np.matmul(Transform,Mean)
+    Cov_pivot = np.matmul(Transform,np.matmul(Cov,Transform.T))
+    print('DE FOM: ', 1/(np.sqrt(Cov_pivot[0,0]*Cov_pivot[1,1])))
+    de_fom = 1/(np.sqrt(Cov_pivot[0,0]*Cov_pivot[1,1]))
+
+    return z_pivot, de_fom
+
+
+def twoD_area(emcee_chain,param_idxs,burnin,num_gridpoints=100):
+    # written with help from chatGPT
+
+    # Extract 2D samples
+    num_params = emcee_chain.shape[2]
+    chain = emcee_chain[:,burnin:,:].reshape((-1,num_params))
+    x = chain[:, param_idxs[0]]
+    y = chain[:, param_idxs[1]]
+
+    # Step 2: KDE estimation over a grid
+    kde = gaussian_kde(np.vstack([x, y]))
+
+    # Create a grid
+    xmin, xmax = x.min(), x.max()
+    ymin, ymax = y.min(), y.max()
+    X, Y = np.meshgrid(np.linspace(xmin,xmax,num_gridpoints),
+        np.linspace(ymin,ymax,num_gridpoints))
+    positions = np.vstack([X.ravel(), Y.ravel()])
+    Z = kde(positions).reshape(X.shape)
+
+    # Step 3: Find the 68% highest posterior density (HPD) threshold
+    Z_flat = Z.flatten()
+    Z_sorted = np.sort(Z_flat)[::-1]
+    cumulative = np.cumsum(Z_sorted)
+    cumulative /= cumulative[-1]
+
+    # Find the density value at 68% cumulative probability
+    threshold_index = np.searchsorted(cumulative, 0.68)
+    density_threshold = Z_sorted[threshold_index]
+
+    # Step 4: Binary mask of the 68% region
+    mask = Z >= density_threshold
+
+    # Step 5: Estimate area using pixel counting
+    dx = (xmax - xmin) / num_gridpoints
+    dy = (ymax - ymin) / num_gridpoints
+    pixel_area = dx * dy
+    area_68 = mask.sum() * pixel_area
+
+    return area_68
+
+
+def median_and_uncertainty(emcee_chain,burnin):
+
+    num_params = emcee_chain.shape[2]
+    chain = emcee_chain[:,burnin:,:].reshape((-1,num_params))
+
+    med = np.median(chain,axis=0)
+    low = np.quantile(chain,q=0.1586,axis=0)
+    high = np.quantile(chain,q=0.8413,axis=0)
+
+    for i,post_med in enumerate(med):
+        print(np.around(post_med,3), ' + ', np.around(high[i]-post_med,3), ' - ', np.around(post_med-low[i],3))
+        arviz_hdi = az.hdi(chain[:,i], hdi_prob=.68)
+        #print('arviz hdi: ', arviz_hdi[1] - arviz_hdi[0])
+        print('arviz (68 hdi / 2)', 
+            np.round((arviz_hdi[1] - arviz_hdi[0])/2, 3))
+        if i == 1:
+            break
+
+
 def analyze_chains(emcee_chain,param_labels,true_hyperparameters,
                     outfile,show_chains=False,
                     burnin=int(1e3)):
